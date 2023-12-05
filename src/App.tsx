75fbfdc98ad96a3c62c8c86457509d0c2a7c41f5
Guillaume Jolibois <97659288+Joliwood@users.noreply.github.com>
2023-12-05 19:53:33 +0100

    :tada: Design background (#9)
    * 🎉 Vitest installed (#2)
    
    * :tada: Full init (#1)
    
    * construction: WIP
    
    * :tada: all init OK with radix UI
    
    * :tada: Vitest Installed
    
    ---------
    
    Co-authored-by: Guillaume Jolibois <97659288+Joliwood@users.noreply.github.com>
    
    * Revert "🎉 Vitest installed (#2)" (#3)
    
    This reverts commit 34a7ffdf9bfccb91e61ce2df2f588046b3d46fc2.
    
    * :tada: background added
    
    * :zap: missed traductions added + animated background improved
    
    ---------
    
    Co-authored-by: Anthony Bourret <127198152+AnthonyBourret@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React, { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/homePage/Home';
 import Listen from './components/Listen';
+import Background from './components/Background';
 
 export default function App() {
   return (
     <Suspense fallback="...is loading">
+      <Background />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/listen" element={<Listen />} />
---
+++ src/components/Background.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Background() {
+  return (
+    <>
+      <div className="bg" />
+      <div className="bg bg2" />
+      <div className="bg bg3" />
+    </>
+  );
+}
+
+export default Background;

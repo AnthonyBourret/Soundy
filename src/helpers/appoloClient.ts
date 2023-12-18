a9fbfe7ec791f9e9ad47296b7f552b19ad2cefb9
Guillaume Jolibois <97659288+Joliwood@users.noreply.github.com>
2023-12-18 20:32:59 +0100

    :zap: new config for dotenv vite

--- src/helpers/appoloClient.ts
+++ src/helpers/appoloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3000/graphql',
+  uri: import.meta.env.VITE_APPOLO_URL,
   cache: new InMemoryCache(),
 });
 

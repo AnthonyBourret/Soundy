f74467b923176875d96373269e41e25d3d20e0fc
Anthony Bourret <127198152+AnthonyBourret@users.noreply.github.com>
2023-12-19 11:41:01 +0100

    :tada: Scroll To Top Button (#13)

--- src/components/favoritesPage/Favorites.tsx
+++ src/components/favoritesPage/Favorites.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../header/Header';
+import ScrollToTopButton from '../customElements/ScrollToTopButton';
 
 function Favorites({ isLogin }: { isLogin: boolean }) {
   return (
     <div className="mb-5 flex flex-col items-center w-full min-h-screen">
       <Header isLogin={isLogin} />
       <p>Favorites</p>
+      <ScrollToTopButton />
     </div>
   );
 }

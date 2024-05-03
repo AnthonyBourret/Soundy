a799169d8f00204557a6d768db74afdd8d8dff6e
Anthony Bourret <anthony.bourret@gmail.com>
2024-05-03 15:56:54 +0200

    :construction: reviewed display on Add song

--- src/components/createPage/CreateSong.tsx
+++ src/components/createPage/CreateSong.tsx
@@ -4,54 +4,64 @@ import { useTranslation } from 'react-i18next';
 function CreateSong() {
   const { t } = useTranslation('translation');
   return (
-    <div className="flex flex-col gap-8 w-full mb-20 px-8 min-[520px]:px-14 min-[620px]:w-3/4 min-[1000px]:w-1/2 min-[1500px]:w-1/3">
-      {/* Title & Upload Files Inputs */}
-      <div className="flex flex-col gap-4">
-        <div className="flex flex-col gap-4">
-          <label htmlFor="title" className="form-control">
-            <div className="label pb-4">
-              <span className="label-text text-lg font-semibold">{t('CREATE_SONG_TITLE_INPUT')}</span>
-            </div>
-            <input type="text" id="title" className="input border border-stone-700 bg-neutral" />
-          </label>
-          <label htmlFor="file" className="form-control">
-            <div className="label pb-4">
-              <span className="label-text text-lg font-semibold">{t('CREATE_SONG_FILE_INPUT')}</span>
-            </div>
-            <input type="file" id="file" placeholder="" className="file-input border border-stone-700 w-full bg-neutral" disabled />
-            <span className="label-text-alt m-1">{t('CREATE_SONG_FILE_LABEL')}</span>
-          </label>
+    <div className="flex flex-col gap-14 border border-stone-700 rounded-box bg-neutral mx-6 mb-20 p-8 min-[820px]:w-3/5 min-[820px]:px-24 min-[800px]:py-12 min-[1200px]:w-2/5">
+
+      <label className="form-control" htmlFor="title">
+        <div className="label my-2">
+          <span className="label-text text-2xl  font-semibold">{t('CREATE_SONG_TITLE_INPUT')}</span>
+        </div>
+        <input type="text" placeholder="..." className="input input-bordered w-full" />
+      </label>
+
+      <label className="form-control" htmlFor="title">
+        <div className="label my-2">
+          <span className="label-text text-2xl font-semibold">{t('CREATE_SONG_FILE_INPUT')}</span>
+        </div>
+        <input
+          type="file"
+          accept=".mp3, .wav"
+          className="file-input file-input-bordered w-full"
+          // disabled
+        />
+        <div className="label self-center">
+          <span className="label-text-alt">{t('CREATE_SONG_FILE_LABEL')}</span>
+        </div>
+      </label>
+
+      <label className="form-control min-[800px]:gap-8" htmlFor="cover">
+        <div className="label mb-4">
+          <span className="label-text text-2xl font-semibold">{t('CREATE_SONG_COVER_INPUT')}</span>
         </div>
 
-        <label htmlFor="artist" className="form-control">
-          <div className="label self-start mb-8">
-            <span className="label-text text-lg font-semibold">{t('CREATE_SONG_COVER_INPUT')}</span>
+        <figure className="w-1/2 rounded-box overflow-hidden self-center min-[1300px]:w-1/3">
+          <img src="/cover-placeholder.png" alt="default_cover" />
+        </figure>
+        <div className="flex flex-col">
+          <input
+            type="file"
+            accept=".jpeg, .jpg, .png"
+            className="file-input file-input-bordered mt-4 w-full"
+          />
+          <div className="label self-center">
+            <span className="label-text-alt">{t('CREATE_SONG_COVER_LABEL')}</span>
           </div>
-          <div className="flex flex-col items-center gap-1 mb-4">
-            <figure className="w-1/2 rounded-box overflow-hidden min-[520px]:w-1/3">
-              <img src="/cover-placeholder.png" alt="cover" />
-            </figure>
-            <span className="label-text-alt">{t('CREATE_SONG_COVER_LABEL_1')}</span>
-          </div>
-          <input type="file" className="file-input border border-stone-700 w-full bg-neutral" disabled />
-          <span className="label-text-alt m-1">{t('CREATE_SONG_COVER_LABEL_2')}</span>
-        </label>
-      </div>
-
-      {/* Lyrics Input */}
-      <label htmlFor="lyrics" className="form-control">
-        <div className="label pb-4">
-          <span className="label-text text-lg font-semibold">{t('CREATE_SONG_LYRICS_INPUT')}</span>
         </div>
-        <textarea className="textarea border border-stone-700 bg-neutral w-full h-[150px]" placeholder={t('CREATE_SONG_LYRICS_PLACEHOLDER')} />
+      </label>
+
+      <label htmlFor="lyrics">
+        <div className="label my-2">
+          <span className="label-text text-2xl font-semibold">{t('CREATE_SONG_LYRICS_INPUT')}</span>
+        </div>
+        <textarea className="textarea textarea-bordered w-full" placeholder={t('CREATE_SONG_LYRICS_PLACEHOLDER')} />
       </label>
 
       <button
-        type="submit"
-        className="btn btn-lg btn-neutral border border-stone-700 py-4 w-fit self-center"
+        type="button"
+        className="btn btn-primary w-fit self-center py-3 text-xl"
       >
         {t('CREATE_SONG_BTN')}
       </button>
+
     </div>
   );
 }

1783dfbd15cff414c237f0613c992e02f4017d78
Guillaume Jolibois <jolibois.contact@gmail.com>
2024-06-03 22:46:33 +0200

    :zap: case sensitive fix

--- src/svg/index.ts
+++ src/svg/index.ts
@@ -1,12 +1,12 @@
 export { default as AddIconChecked } from './AddIconChecked';
 export { default as AddIconUnchecked } from './AddIconUnchecked';
 export { default as ArrowDown } from './ArrowDown';
-export { default as Logo } from './logo';
+export { default as Logo } from './Logo';
 export { default as PencilIcon } from './PencilIcon';
 export { default as PlayerPauseIcon } from './PlayerPauseIcon';
 export { default as PlayerPlayIcon } from './PlayerPlayIcon';
 export { default as PlayerPrevNextIcon } from './PlayerPrevNextIcon';
-export { default as PlayIcon } from './playIcon';
-export { default as PlusIcon } from './plusIcon';
-export { default as SoundIcon } from './soundIcon';
+export { default as PlayIcon } from './PlayIcon';
+export { default as PlusIcon } from './PlusIcon';
+export { default as SoundIcon } from './SoundIcon';
 export { default as UploadIcon } from './UploadIcon';

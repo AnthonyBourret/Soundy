3f2f0a3e49067cbd422d8928af998a7934ceeb22
Guillaume Jolibois <97659288+Joliwood@users.noreply.github.com>
2024-06-15 14:09:12 +0200

    :tada: Infinite scroll (#46)
    * :tada: pagination added but with ugly code
    
    * :tada: scroll converted to inifinit scroll

--- src/components/listenPage/Listen.tsx
+++ src/components/listenPage/Listen.tsx
@@ -1,18 +1,22 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, {
+  useState, useEffect, useMemo, useRef, useCallback,
+} from 'react';
 import { useQuery } from '@apollo/client';
+
 import Header from '../header/Header';
-import SongDisplay from './SongDisplay';
-import AlbumDisplay from './AlbumDisplay';
 import { ScrollToTopButton, SongAndAlbumOrder, Spinner } from '../customElements';
 import { ChosenDisplay } from '../../types';
-import SearchBar from './SearchBar';
 import { ListenPageSongsQuery } from '../../requests/queries';
 import type {
-  ListenPageSongsQueryQuery,
-  ListenPageAlbumsQueryQuery,
   DurationRange,
+  ListenPageAlbumsQueryQuery,
+  ListenPageSongsQueryQuery,
   ReleaseYear,
 } from '../../types/__generated_schemas__/graphql';
+
+import SearchBar from './SearchBar';
+import AlbumDisplay from './AlbumDisplay';
+import SongDisplay from './SongDisplay';
 
 function Listen({ isLogin }: { isLogin: boolean }) {
   const [songs, setSongs] = useState<ListenPageSongsQueryQuery['songs']>([]);
@@ -21,26 +25,56 @@ function Listen({ isLogin }: { isLogin: boolean }) {
   const [sortBy, setSortBy] = useState<string | null>(null);
   const [durationFilter, setDurationFilter] = useState<DurationRange>();
   const [yearFilter, setYearFilter] = useState<ReleaseYear>();
+  const observerRef = useRef<IntersectionObserver | null>(null);
+  const loadMoreRef = useRef<HTMLDivElement | null>(null);
 
-  const { data, loading, error } = useQuery(
+  const {
+    data, loading, error, fetchMore,
+  } = useQuery<ListenPageSongsQueryQuery>(
     ListenPageSongsQuery,
     {
-      variables: { limit: 30 },
+      variables: { offset: 0, limit: 30 },
       fetchPolicy: 'network-only',
     },
   );
-
-  const resetFilters = () => {
-    setSortBy(null);
-    setDurationFilter(undefined);
-    setYearFilter(undefined);
-  };
 
   useEffect(() => {
     if (data?.songs !== undefined) {
       setSongs(data.songs);
     }
   }, [data]);
+
+  const loadMore = useCallback(() => {
+    fetchMore({
+      variables: {
+        offset: songs?.length,
+      },
+      updateQuery: (prev, { fetchMoreResult }) => {
+        if (!fetchMoreResult) return prev;
+        return {
+          songs: [...prev.songs as any, ...fetchMoreResult.songs as any],
+        };
+      },
+    });
+  }, [fetchMore, songs]);
+
+  useEffect(() => {
+    if (observerRef.current) observerRef.current.disconnect();
+
+    observerRef.current = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        loadMore();
+      }
+    });
+
+    if (loadMoreRef.current) {
+      observerRef.current.observe(loadMoreRef.current);
+    }
+
+    return () => {
+      if (observerRef.current) observerRef.current.disconnect();
+    };
+  }, [loadMore]);
 
   const songOrAlbumJSX = useMemo(() => {
     if (loading) {
@@ -56,13 +90,10 @@ function Listen({ isLogin }: { isLogin: boolean }) {
     }
 
     if (chosenDisplay === 'albums') {
-      return (
-        <AlbumDisplay albums={albums} sortBy={sortBy} />
-      );
+      return <AlbumDisplay albums={albums} sortBy={sortBy} />;
     }
-    return (
-      <SongDisplay songs={songs} isLogin={isLogin} sortBy={sortBy} />
-    );
+
+    return <SongDisplay songs={songs} isLogin={isLogin} sortBy={sortBy} />;
   }, [albums, chosenDisplay, error, isLogin, loading, songs, sortBy]);
 
   return (
@@ -71,17 +102,19 @@ function Listen({ isLogin }: { isLogin: boolean }) {
       <SearchBar
         chosenDisplay={chosenDisplay}
         setChosenDisplay={setChosenDisplay}
-        setSongs={setSongs}
+        setSongs={() => {}}
+        resetFilters={() => {}}
         setAlbums={setAlbums}
         setYearFilter={setYearFilter}
         setDurationFilter={setDurationFilter}
         yearFilter={yearFilter}
         durationFilter={durationFilter}
-        resetFilters={resetFilters}
       />
       <SongAndAlbumOrder sortBy={sortBy} setSortBy={setSortBy} chosenDisplay={chosenDisplay} />
 
       {songOrAlbumJSX}
+
+      <div ref={loadMoreRef} className="observer-element" />
 
       <ScrollToTopButton />
     </div>
